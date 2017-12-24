8e39cfc5de9dba479a237d4374c307d4040accf1
Imisioluwa Akande <imisioluwa.akande@andela.com>
2017-12-25 00:52:22 +0100

    modify path settings

--- meelorlah/meelorlah/settings.py
+++ meelorlah/meelorlah/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '../static/meelorlah/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 LOGIN_REDIRECT_URL = "test"
 LOGOUT_REDIRECT_URL = "thanks"
